9711a940d32a12226004bf6168b70570c248cc58
vadymrass <vadymrass0509@gmail.com>
2023-07-28 18:45:52 +0300

    added alert function to SyncData function: Task 9

--- utils/sync/SyncData.tsx
+++ utils/sync/SyncData.tsx
@@ -8,4 +8,5 @@ export default async function SyncData() {
     console.log(JSON.parse(localStorage.getItem("formdata") as string))
     console.log("The SyncData function has been triggered")
     console.log("The number of updated records", (JSON.parse(localStorage.getItem("formdata") as string)).length)
+    window.alert(`The currend ResultData ${localStorage.getItem("resultdata")}`);
 }
